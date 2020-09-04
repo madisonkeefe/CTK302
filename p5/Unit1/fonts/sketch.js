9fb93257ad64cd06b3a576a750360217d7085552
madisonkeefe <madisonmkeefe@gmail.com>
2020-09-03 20:15:13 -0500

    push

--- p5/Unit1/fonts/sketch.js
+++ p5/Unit1/fonts/sketch.js
@@ -1,17 +1,19 @@
-var f, f1;
+var f, f1, img3;
 
 function setup() {
   createCanvas(800, 800);
 
   f = loadFont('assets/a.ttf');
   f1 = loadFont('assets/s.ttf');
+  img3 = loadImage("assets/3.jpg");
 
   textAlign(CENTER);
+  imageMode(CENTER);
 }
 
 function draw() {
-  background(100);
-
+  background('white');
+  image(img3, width / 2, height / 2);
   textFont(f1);
   fill('white');
   textSize(20);
